40ef101be0ef4e8e96b344191ec72337e4417158
Arsen <arsen.simonian@haqqex.com>
2025-11-22 19:33:00 +0400

    ufc

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  i18n: {
-    locales: ['en', 'es', 'ru', 'pt', 'fr', 'de', 'ar', 'hy'],
-    defaultLocale: 'en',
-    localeDetection: true,
-  },
   images: {
     remotePatterns: [
       {
